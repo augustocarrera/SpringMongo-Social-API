18b5696c50c73db7b1e6b2f12fdae80bb995693d
Augusto Carrera <augusto.carrera@hotmail.com>
2025-12-10 11:11:27 -0300

    Referenciando os posts do usuário

--- src/main/java/com/augustocarrera/workshopmongo/domain/User.java
+++ src/main/java/com/augustocarrera/workshopmongo/domain/User.java
@@ -1,9 +1,11 @@
 package com.augustocarrera.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -13,6 +15,17 @@ public class User implements Serializable{
     private String id;
     private String name;
     private String email;
+
+    @DBRef (lazy = true)
+    private List<Post> posts = new ArrayList<>();
+    
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 
     public User(){      
     }
